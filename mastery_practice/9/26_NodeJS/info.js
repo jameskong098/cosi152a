20a598c461cedbe59592edcc5edd3738e29bdd14
Jimmy Kong <jameskong098@gmail.com>
2023-09-26 19:14:18 -0400

    add mastery practice

---
+++ mastery_practice/9/26_NodeJS/info.js
@@ -0,0 +1,5 @@
+let name = "Johm Smith";
+let address = "Brandeis University, Waltham, MA 02453";
+
+const numSum = require("./sumNum")
+console.log(numSum.addNums(16,8))
---
+++ mastery_practice/9/26_NodeJS/sumNum.js
@@ -0,0 +1,5 @@
+function addNums(x, y) {
+    return x + y;
+}
+
+module.exports = { addNums };
